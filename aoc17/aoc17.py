01d70a678f3de77c12d59b8922534adfad34e023
atmoe <18740986+atmoe@users.noreply.github.com>
2020-12-17 21:39:43 -0600

    Day 17 consolidated
    Consolidate p1 and p2 scripts into one script.

--- aoc17/aoc17.py
+++ aoc17/aoc17.py
@@ -3,92 +3,144 @@
 import sys
 import re
 import copy
+import numpy as np
 
 debug = False
 def dbgPrint(string, end='\n'):
     if debug: print(string, end=end)
 
-def printGrid(g):
+def print3DGrid(g):
     for z in range(len(g)):
         for y in range(len(g[0])):
             print("".join(g[z][y]))
         print()
 
-def increaseWidth(g, w, h, d):
-    for z in range(d):
-        for y in range(h):
-            g[z][y].append('.')
-            g[z][y].insert(0, '.')
+def print4DGrid(g):
+    for w in range(len(g)):
+        for z in range(len(g[0])):
+            print(f'z={z} w={w}')
+            for y in range(len(g[0][0])):
+                print("".join(g[w][z][y]))
+            print()
 
-def increaseHeight(g, w, h, d):
-    for z in range(d):
-        g[z].append(['.']*w)
-        g[z].insert(0, ['.']*w)
-
-def increaseDepth(g, w, h, d):
-    newSlice = []
-    for y in range(h):
-        newSlice.append(['.']*w)
-
-    g.append(newSlice)
-    g.insert(0, copy.deepcopy(newSlice))
-
-def getCount(g, x, y, z, w, h, d):
+def get3DCount(g, x, y, z, dims):
     count = 0
     for z_off in [-1, 0, 1]:
         for y_off in [-1, 0, 1]:
             for x_off in [-1, 0, 1]:
                 if x_off==0 and y_off==0 and z_off==0: continue
-                if (z+z_off) < 0 or (z+z_off) >= d: continue
-                if (y+y_off) < 0 or (y+y_off) >= h: continue
-                if (x+x_off) < 0 or (x+x_off) >= w: continue
+                if (z+z_off) < 0 or (z+z_off) >= dims[-3]: continue
+                if (y+y_off) < 0 or (y+y_off) >= dims[-2]: continue
+                if (x+x_off) < 0 or (x+x_off) >= dims[-1]: continue
 
                 if g[z+z_off][y+y_off][x+x_off] == '#':
                     count+=1
     return count
 
+def get4DCount(g, x, y, z, w, dims):
+    count = 0
+    for w_off in [-1, 0, 1]:
+        for z_off in [-1, 0, 1]:
+            for y_off in [-1, 0, 1]:
+                for x_off in [-1, 0, 1]:
+                    if x_off==0 and y_off==0 and z_off==0 and w_off==0: continue
+                    if (w+w_off) < 0 or (w+w_off) >= dims[-4]: continue
+                    if (z+z_off) < 0 or (z+z_off) >= dims[-3]: continue
+                    if (y+y_off) < 0 or (y+y_off) >= dims[-2]: continue
+                    if (x+x_off) < 0 or (x+x_off) >= dims[-1]: continue
+
+                    if g[w+w_off][z+z_off][y+y_off][x+x_off] == '#':
+                        count+=1
+    return count
+
 assert len(sys.argv) == 2, sys.argv[0] + " requires 1 argument!"
 
-grid = [[]]
+
+def increaseDim(g, d, axis, direction):
+    xS = 0
+    xE = d[-1]
+    yS = 0
+    yE = d[-2]
+    zS = 0
+    zE = d[-3]
+    if len(d) == 4:
+        wS = 0
+        wE = d[-4]
+    if axis == 'width':
+        d[-1] += 1
+        if direction == -1:
+            xS += 1
+            xE += 1
+    if axis == 'height':
+        d[-2] += 1
+        if direction == -1:
+            yS += 1
+            yE += 1
+    if axis == 'depth':
+        d[-3] += 1
+        if direction == -1:
+            zS += 1
+            zE += 1
+    if axis == 'fourth':
+        d[-4] += 1
+        if direction == -1:
+            wS += 1
+            wE += 1
+    newGrid = np.chararray(shape=d, unicode=True)
+    newGrid.fill('.')
+
+    if len(d) == 3:
+        newGrid[zS:zE,yS:yE,xS:xE] = g
+    if len(d) == 4:
+        newGrid[wS:wE,zS:zE,yS:yE,xS:xE] = g
+
+    return newGrid
+
+grid2D = []
 inputFile = open(sys.argv[1], "r")
 for line in inputFile.readlines():
-    grid[0].append(list(line.strip()))
-
+    grid2D.append(list(line.strip()))
 inputFile.close()
 
-print("--- Initial Grid ---")
-printGrid(grid)
 
 print("------------------")
 print("---- PART 1 ------")
 print("------------------")
 
-numIterations = 6
-gW = len(grid[0][0])
-gH = len(grid[0])
-gD = len(grid)
+dims = [1, len(grid2D), len(grid2D[0])] # Depth, Height, Width
+grid = np.chararray(shape=dims, unicode=True)
+grid.fill('.')
+for y in range(len(grid2D)):
+    for x in range(len(grid2D[0])):
+            grid[0][y][x] = grid2D[y][x]
 
-# expand inactive around grid
-increaseWidth(grid, gW, gH, gD)
-gW+=2
-increaseHeight(grid, gW, gH, gD)
-gH+=2
-increaseDepth(grid, gW, gH, gD)
-gD+=2
+print("--- Initial Grid ---")
+print3DGrid(grid)
 
 print("--- Padded Grid ---")
-printGrid(grid)
+grid = increaseDim(grid, dims, 'width',   1)
+grid = increaseDim(grid, dims, 'width',  -1)
+grid = increaseDim(grid, dims, 'height',  1)
+grid = increaseDim(grid, dims, 'height', -1)
+grid = increaseDim(grid, dims, 'depth',   1)
+grid = increaseDim(grid, dims, 'depth',  -1)
+print3DGrid(grid)
+
+numIterations = 6
 
 nextGrid = copy.deepcopy(grid)
 for i in range(numIterations):
     print(f"--- Cycle {i+1} ---")
-    expandW=False
-    expandH=False
-    expandD=False
-    for z in range(gD):
-        for y in range(gH):
-            for x in range(gW):
-                count = getCount(grid, x, y, z, gW, gH, gD)
+    expandW_pos=False
+    expandW_neg=False
+    expandH_pos=False
+    expandH_neg=False
+    expandD_pos=False
+    expandD_neg=False
+    for z in range(dims[0]):
+        for y in range(dims[1]):
+            for x in range(dims[2]):
+                count = get3DCount(grid, x, y, z, dims)
                 if grid[z][y][x] == '.' and count == 3:
                     nextGrid[z][y][x] = '#'
                 elif grid[z][y][x] == '#' and (count == 2 or count == 3):
@@ -97,35 +149,112 @@ for i in range(numIterations):
                     nextGrid[z][y][x] = '.'
 
                 if nextGrid[z][y][x] == '#':
-                    if z==0 or z==gD-1: expandD = True
-                    if y==0 or y==gH-1: expandH = True
-                    if x==0 or x==gW-1: expandW = True
+                    if z==0:          expandD_neg = True
+                    if y==0:          expandH_neg = True
+                    if x==0:          expandW_neg = True
+                    if z==dims[-3]-1: expandD_pos = True
+                    if y==dims[-2]-1: expandH_pos = True
+                    if x==dims[-1]-1: expandW_pos = True
 
-    if expandW:
-        increaseWidth(nextGrid, gW, gH, gD)
-        gW+=2
-    if expandH:
-        increaseHeight(nextGrid, gW, gH, gD)
-        gH+=2
-    if expandD:
-        increaseDepth(nextGrid, gW, gH, gD)
-        gD+=2
+    if expandW_pos: nextGrid = increaseDim(nextGrid, dims, 'width',   1)
+    if expandW_neg: nextGrid = increaseDim(nextGrid, dims, 'width',  -1)
+    if expandH_pos: nextGrid = increaseDim(nextGrid, dims, 'height',  1)
+    if expandH_neg: nextGrid = increaseDim(nextGrid, dims, 'height', -1)
+    if expandD_pos: nextGrid = increaseDim(nextGrid, dims, 'depth',   1)
+    if expandD_neg: nextGrid = increaseDim(nextGrid, dims, 'depth',  -1)
 
-    printGrid(nextGrid)
+    print3DGrid(nextGrid)
     grid = copy.deepcopy(nextGrid)
 
 totalActive = 0
-for z in range(gD):
-    for y in range(gH):
-        for x in range(gW):
+for z in range(dims[-3]):
+    for y in range(dims[-2]):
+        for x in range(dims[-1]):
             if grid[z][y][x] == '#':
                 totalActive+=1
 
 print(f'Total Active = {totalActive}')
 
 
-
 print("------------------")
 print("---- PART 2 ------")
 print("------------------")
+dims = [1, 1, len(grid2D), len(grid2D[0])] # Depth, Height, Width
+grid = np.chararray(shape=dims, unicode=True)
+grid.fill('.')
+for y in range(len(grid2D)):
+    for x in range(len(grid2D[0])):
+        grid[0][0][y][x] = grid2D[y][x]
+
+print("--- Initial Grid ---")
+print4DGrid(grid)
+
+print("--- Padded Grid ---")
+grid = increaseDim(grid, dims, 'width',   1)
+grid = increaseDim(grid, dims, 'width',  -1)
+grid = increaseDim(grid, dims, 'height',  1)
+grid = increaseDim(grid, dims, 'height', -1)
+grid = increaseDim(grid, dims, 'depth',   1)
+grid = increaseDim(grid, dims, 'depth',  -1)
+grid = increaseDim(grid, dims, 'fourth',  1)
+grid = increaseDim(grid, dims, 'fourth', -1)
+print4DGrid(grid)
+
+numIterations = 6
+
+nextGrid = copy.deepcopy(grid)
+for i in range(numIterations):
+    print(f"--- Cycle {i+1} ---")
+    expandW_pos=False
+    expandW_neg=False
+    expandH_pos=False
+    expandH_neg=False
+    expandD_pos=False
+    expandD_neg=False
+    expand4_pos=False
+    expand4_neg=False
+    for w in range(dims[-4]):
+        for z in range(dims[-3]):
+            for y in range(dims[-2]):
+                for x in range(dims[-1]):
+                    count = get4DCount(grid, x, y, z, w, dims)
+                    if grid[w][z][y][x] == '.' and count == 3:
+                        nextGrid[w][z][y][x] = '#'
+                    elif grid[w][z][y][x] == '#' and (count == 2 or count == 3):
+                        nextGrid[w][z][y][x] = '#'
+                    else:
+                        nextGrid[w][z][y][x] = '.'
+
+                    if nextGrid[w][z][y][x] == '#':
+                        if w==0:          expand4_neg = True
+                        if z==0:          expandD_neg = True
+                        if y==0:          expandH_neg = True
+                        if x==0:          expandW_neg = True
+                        if z==dims[-4]-1: expand4_pos = True
+                        if z==dims[-3]-1: expandD_pos = True
+                        if y==dims[-2]-1: expandH_pos = True
+                        if x==dims[-1]-1: expandW_pos = True
+
+    if expandW_pos: nextGrid = increaseDim(nextGrid, dims, 'width',   1)
+    if expandW_neg: nextGrid = increaseDim(nextGrid, dims, 'width',  -1)
+    if expandH_pos: nextGrid = increaseDim(nextGrid, dims, 'height',  1)
+    if expandH_neg: nextGrid = increaseDim(nextGrid, dims, 'height', -1)
+    if expandD_pos: nextGrid = increaseDim(nextGrid, dims, 'depth',   1)
+    if expandD_neg: nextGrid = increaseDim(nextGrid, dims, 'depth',  -1)
+    if expand4_pos: nextGrid = increaseDim(nextGrid, dims, 'fourth',  1)
+    if expand4_neg: nextGrid = increaseDim(nextGrid, dims, 'fourth', -1)
+
+    print4DGrid(nextGrid)
+    grid = copy.deepcopy(nextGrid)
+
+totalActive = 0
+for w in range(dims[-4]):
+    for z in range(dims[-3]):
+        for y in range(dims[-2]):
+            for x in range(dims[-1]):
+                if grid[w][z][y][x] == '#':
+                    totalActive+=1
+
+print(f'Total Active = {totalActive}')
+
 
